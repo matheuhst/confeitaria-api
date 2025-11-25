e5f47ca88ed8b311256700134a6092500e244f0c
matheuhst <mmmatheus.fsouza@gmail.com>
2025-11-25 19:30:28 -0300

    Add files via upload

---
+++ cardapio.py
@@ -0,0 +1,21 @@
+def buscar_cardapio():
+    itens = [
+        {
+            "id": 1,
+            "nome": "Chocomoça",
+            "descrição": "Bolo de chocolate chocante (para mocas).",
+            "preço": 49.99,
+            "foto": "shop"
+        }
+    ]
+
+    return itens
+
+def buscar_por_id(item_id):
+    return{
+        "id": 1,
+        "nome": "Chocomoça",
+        "descrição": "Bolo de chocolate chocantees (para mocas).",
+        "preço": 49.99,
+        "foto": "shop"
+    }
---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from cardapio import buscar_cardapio, buscar_por_id
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return{
+    "message":"Api rodando"
+  }
+
+@app.route("/cardapio")
+def cardapio():
+  return buscar_cardapio()
+
+@app.route("/cardapio/<int:item_id>")
+def cardapio_por_id(item_id):
+  return buscar_por_id(item_id)
+
+if __name__ == "__main__":
+  app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
